98972f8465a455dbf22e16b28d47a59cd83ff417
afrabouzehar <bouzehar.afra@gmail.com>
2025-11-23 21:57:26 +0100

    FIRSTPY

---
+++ 1/1.py
@@ -0,0 +1,7 @@
+a = int(input("Entrez un entier : "))
+b = int(input("Entrez un autre entier : "))
+
+produit = a * b
+
+print("Le produit de", a, "par", b, "est", produit)
+# Ce programme demande à l'utilisateur de saisir deux entiers
