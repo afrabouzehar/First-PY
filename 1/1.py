c7ce81249058a627db3b0e498412f4b47989226b
afrabouzehar <bouzehar.afra@gmail.com>
2025-11-23 22:31:01 +0100

    1.py

--- 1/1.py
+++ 1/1.py
@@ -4,3 +4,5 @@ b = int(input("Entrez un autre entier : "))
 produit = a * b
 
 print("Le produit de", a, "par", b, "est", produit)
+# Ce programme demande à l'utilisateur de saisir deux entiers,
+# calcule leur produit, puis affiche le résultat.
