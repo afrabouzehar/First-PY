1693463602bddc23abc20f125be9a54a872387bb
afrabouzehar <bouzehar.afra@gmail.com>
2025-11-24 06:16:50 +0100

    nEw

--- 1/2.py
+++ 1/2.py
@@ -1,3 +1,4 @@
 Val = float(input("Entrez la valeur de x : "))
 Result = (Val ** 2 - (3 * Val)+ 0.5) * (Val - 3)
 print("Le résultat de f(x) pour x =",Val, "est  : ", Result)   
+#Affichage de la valeur de f(x)=(x^2 - 3x + 0.5)(x - 3) pour une valeur x donnée par l'utilisateur.
