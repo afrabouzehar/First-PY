410e2f39a54b135aed51f9c1216706c4d3ec3eae
afrabouzehar <bouzehar.afra@gmail.com>
2025-11-24 08:56:19 +0100

    "UnEw"

--- 1/firstprogramm.py
+++ 1/firstprogramm.py
@@ -3,6 +3,6 @@ b = int(input("Entrez un autre entier : "))
 
 produit = a * b
 
-print("Le produit de", a, "par", b, "est", produit)
+print("Le produit de", a, "par", b, "est :", produit)
 # Ce programme demande à l'utilisateur de saisir deux entiers,
 # calcule leur produit, puis affiche le résultat.
