c708cb49f21cb91eee65738a517c2ad224802e43
afrabouzehar <bouzehar.afra@gmail.com>
2025-11-24 10:22:36 +0100

    uPdatE

--- 1/calcul-sal.py
+++ 1/calcul-sal.py
@@ -1,1 +1,7 @@
-nbr_heure
+nbr_heure =int(input("Entrez le nombre d'heures travaillées : "))
+taux_horaire =float (input("Entrez le taux horaire : "))
+Montant_total = nbr_heure * taux_horaire
+print("Le salaire total est de :", Montant_total)
+
+# Ce programme calcule le salaire total en multipliant le nombre d'heures travaillées
+# par le taux horaire, puis affiche le résultat.
