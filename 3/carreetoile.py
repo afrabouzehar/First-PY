a4b6fce98d554c13da0993655b598ee7660303be
afrabouzehar <bouzehar.afra@gmail.com>
2025-12-01 11:10:24 +0100

    Update of HEAD

--- 3/carreetoile.py
+++ 3/carreetoile.py
@@ -1,6 +1,6 @@
-n = int(input("Entrez un nombre n : "))
+n = int(input("Entrez un nombre n :" ))
 carre_etoile = 0
 for i in range(n):
     carre_etoile = n*n
-    print("*" * n)
+    print("*  " * n)
     
