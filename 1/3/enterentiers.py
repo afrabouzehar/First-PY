c101598022a3b2485a027ba4c9cc0034219be725
afrabouzehar <bouzehar.afra@gmail.com>
2025-12-01 09:16:21 +0100

    oui

---
+++ 1/3/enterentiers.py
@@ -0,0 +1,13 @@
+nbr = int(input("Entrez un nombre entier : "))
+somme = 0
+while nbr>= 0:
+    somme += nbr
+    nbr = int(input("Entrez un nombre entier : "))
+print("La somme des nombres entiers saisis est :", somme)
+
+# Fin du programme
+
+
+
+
+
