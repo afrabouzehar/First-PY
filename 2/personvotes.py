165ba60a5fe07412fa06801f1b07635e7899dcd8
afrabouzehar <bouzehar.afra@gmail.com>
2025-12-08 09:30:33 +0100

    person votes

---
+++ 2/personvotes.py
@@ -0,0 +1,14 @@
+Liste_Noms=["Yasmine", "Amine","Yasmine", "Omar", "Yasmine", "Amine", "Omar", "Amine", "Yasmine", "Sara", "Omar","Yasmine", "Sara"]
+Nbr_Votes = {}
+for nom in Liste_Noms:
+    if nom in Nbr_Votes:
+        Nbr_Votes[nom] += 1 
+    else:
+        Nbr_Votes[nom] = 1
+print("Resultas des votes :",Nbr_Votes)
+print("Candidat avec le plus de votes :", max(Nbr_Votes, key=Nbr_Votes.get))
+
+
+
+    
+    
