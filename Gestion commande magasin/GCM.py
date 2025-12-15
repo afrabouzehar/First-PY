bcc8c61a0f8586644094e2e2aeba050f29bb7430
afrabouzehar <bouzehar.afra@gmail.com>
2025-12-15 11:14:58 +0100

    Adding commande

--- Gestion commande magasin/GCM.py
+++ Gestion commande magasin/GCM.py
@@ -1,7 +1,21 @@
-commandes = {"cmd1":{"ID":"Ali","produit":[("Huile",23,27),("miel",40,10)]},"cmd2":{"ID":"Ali","produit":[("Souris",10,20),("Pomme",5,30)]}}
+commandes = {"cmd1":{"ID":"Ali","produit":[("Huile",23,27),("miel",40,10)]},"cmd2":{"ID":"Sami","produit":[("Souris",10,20),("Pomme",5,30)]},"cmd3":{"ID":"Anne","produit":[("Tomate",10,20),("Pomme",5,30)]}}
 for i,j in commandes.items():
     print("==== commandes",i,"====")
     print("client",{j["ID"]})
     for nom,p,Qte in j["produit"]:
         print(f"Nom {nom}| Prix_unitaire {p}| Quantite {Qte}")
-   
+for i,j in commandes.items():
+    client = j["ID"]
+    produit = j["produit"]        
+    T = 0
+    for nom,p,Qte in produit:
+        T = T+(Qte*p)
+    print("==== commandes",i,"====")
+    print(f"client {client}| Somme total {T}")    
+T=0
+for i,j in commandes.items():
+    client = j["ID"]
+    produit = j["produit"]        
+    for nom,p,Qte in produit:
+        T = T+(Qte*p)
+print(f"----chiffre d'affaire {T}----")       
