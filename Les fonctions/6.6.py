164639e6011b1820722a82bd1589006369ce65e0
afrabouzehar <bouzehar.afra@gmail.com>
2025-12-22 11:33:05 +0100

    Les nbrs premiers

--- Les fonctions/6.6.py
+++ Les fonctions/6.6.py
@@ -2,4 +2,7 @@ def decouper_liste(H,n):
     L=[]
     for i in range(0,len(H),n):
         c=H[i:i+n]
-        
+        L.append(c)
+    return L
+Liste=[1,2,3,4,5,6,7,8,9]
+print(decouper_liste(Liste,2))
---
+++ Les fonctions/7.7.py
@@ -0,0 +1,6 @@
+def test_premiers(z):
+    if n<2:
+        return False
+    for i in range (2,7):
+        if z%i:
+            
