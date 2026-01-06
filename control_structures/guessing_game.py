b52b0b3906caab992225ae16e552bc7479b4bfd6
afrabouzehar <bouzehar.afra@gmail.com>
2026-01-06 22:24:07 +0100

    finishing just debugging rn

--- control_structures/guessing_game.py
+++ control_structures/guessing_game.py
@@ -1,32 +1,33 @@
 import random
 secret = random.randint(1, 50)
 attempts = 0
-try:
-while attempts <=7 and secret <=50 :
+while attempts <=7:
     number = input("pick up a number between 1 and 50 (you have 7 attempts): ")
-    if number != int:
-        print("It must be an integer")
+    try:
+        number = int(number)
+    except ValueError:
+        print("Please enter an integer")
         continue
     if number < 1 or number > 50:
         print("Number must be between 1 and 50")
         continue
     if number == secret:
-        print("you win")
+        print("You win")
         break
     elif number >secret:
         attempts += 1
-        print("too high")
+        print("Too high")
         continue
     elif number<secret:
         attempts += 1
-        print("too low")
+        print("Too low")
         continue
     elif abs(number-secret)>1:
         attempts += 1
-        print("very close")
+        print("Very close")
         continue
-    if attempts >= 7:
-        print("Your attempts are done")
-        print("The secret number is : ", secret)
-        break
+while attempts >= 7:
+    print("Your attempts are done")
+    print("The secret number is : ", secret)
+    break
 
