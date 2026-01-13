924c0e1b5c1849e954d0a922e83d32d2c8e2fde4
afrabouzehar <bouzehar.afra@gmail.com>
2026-01-13 18:32:52 +0100

    finisheed ;)

--- control_structures/nested_loops_pattern.py
+++ control_structures/nested_loops_pattern.py
@@ -5,9 +5,20 @@ for i in range(1,5):
 for i in range(4,1,-1):
     for j in range(1,i):
         print('*', end="")
-    print()    
+    print()  
 
+#or like this
 
+lign=0
+for lign in range(1,7):
+    if lign<=4:
+        etoiles=lign
+    else:
+        etoiles=8-lign
+    for j in range(etoiles):
+        print('*', end="")
+    print()
+    
     
        
        
