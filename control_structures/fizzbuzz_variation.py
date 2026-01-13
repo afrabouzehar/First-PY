b80b7bf3a0bb89fafaf500d1008b46206a88f794
afrabouzehar <bouzehar.afra@gmail.com>
2026-01-13 16:00:55 +0100

    finished the fizzbuzz variation

--- control_structures/fizzbuzz_variation.py
+++ control_structures/fizzbuzz_variation.py
@@ -1,4 +1,11 @@
 for i in range(1,101):
     if i%9==0:
         continue
-        
+    elif i % 12==0:
+        print("QuadHex")
+    elif i % 4==0:
+        print("Quad")
+    elif i % 6==0:
+        print("Hex")
+    else:
+        print(i)         
